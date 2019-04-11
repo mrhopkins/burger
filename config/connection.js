26c08a89ddd11615c19b85aacc0b5fe676575a27
Matthew Hopkins <mattrhopkins@gmail.com>
2019-04-11 00:26:45 -0600

    connection.js updated

--- config/connection.js
+++ config/connection.js
@@ -6,10 +6,10 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-    port: 3306,
+    port: 8889,
     host: "localhost",
     user: "root",
-    password: "",
+    password: "root",
     database: "burgers_db"
   })
 }
